1619da609a9314cdd585ff01cb87bb879196ab3c
redhook <redhook62@users.noreply.github.com>
2019-01-24 16:31:24 +0100

    Update 2- build_resources.cmd

--- Neos-IdentityServer 2.3/Manage_Resources/2-Build_resources.cmd
+++ Neos-IdentityServer 2.3/Manage_Resources/2-Build_resources.cmd
@@ -1,22 +1,22 @@
 @ECHO Building Resources files
 
 @ECHO MULTIFACTOR
-resgen Sources\Neos.IdentityServer.MultiFactor\Resources\SErrors.resx resources\Neos.IdentityServer.MultiFactor.Resources.SErrors.resources
-resgen Sources\Neos.IdentityServer.MultiFactor\Resources\SHtml.resx  resources\Neos.IdentityServer.MultiFactor.Resources.SHtml.resources
-resgen Sources\Neos.IdentityServer.MultiFactor\Resources\SInfos.resx  resources\Neos.IdentityServer.MultiFactor.Resources.SInfos.resources
-resgen Sources\Neos.IdentityServer.MultiFactor\Resources\STitle.resx  resources\Neos.IdentityServer.MultiFactor.Resources.STitle.resources
-resgen Sources\Neos.IdentityServer.MultiFactor\Resources\SCheck.resx  resources\Neos.IdentityServer.MultiFactor.Resources.SCheck.resources
+resgen Sources\Neos.IdentityServer.MultiFactor\Resources\SErrors.resx resources\Neos.IdentityServer.MultiFactor.Resources.SErrors.en.resources
+resgen Sources\Neos.IdentityServer.MultiFactor\Resources\SHtml.resx  resources\Neos.IdentityServer.MultiFactor.Resources.SHtml.en.resources
+resgen Sources\Neos.IdentityServer.MultiFactor\Resources\SInfos.resx  resources\Neos.IdentityServer.MultiFactor.Resources.SInfos.en.resources
+resgen Sources\Neos.IdentityServer.MultiFactor\Resources\STitle.resx  resources\Neos.IdentityServer.MultiFactor.Resources.STitle.en.resources
+resgen Sources\Neos.IdentityServer.MultiFactor\Resources\SCheck.resx  resources\Neos.IdentityServer.MultiFactor.Resources.SCheck.en.resources
 
 @ECHO COMMON
 resgen Sources\Neos.IdentityServer.Common\Resources\CSErrors.resx resources\Neos.IdentityServer.MultiFactor.Common.Resources.CSErrors.resources
-resgen Sources\Neos.IdentityServer.Common\Resources\CSHtml.resx resources\Neos.IdentityServer.MultiFactor.Common.Resources.CSHtml.resources
-resgen Sources\Neos.IdentityServer.Common\Resources\CSMail.resx resources\Neos.IdentityServer.MultiFactor.Common.Resources.CSMail.resources
+resgen Sources\Neos.IdentityServer.Common\Resources\CSHtml.resx resources\Neos.IdentityServer.MultiFactor.Common.Resources.CSHtml.en.resources
+resgen Sources\Neos.IdentityServer.Common\Resources\CSMail.resx resources\Neos.IdentityServer.MultiFactor.Common.Resources.CSMail.en.resources
 
 @ECHO AZURE
-resgen Sources\Neos.IdentityServer.MultiFactor.SAS.Azure\Resources\SSAS.resx resources\Neos.IdentityServer.MultiFactor.SAS.Resources.SSAS.resources
+resgen Sources\Neos.IdentityServer.MultiFactor.SAS.Azure\Resources\SSAS.resx resources\Neos.IdentityServer.MultiFactor.SAS.Resources.SSAS.en.esources
 
 @ECHO SMS
-resgen Sources\Neos.IdentityServer.MultiFactor.SMS.Azure\Resources\SAzure.resx resources\Neos.IdentityServer.MultiFactor.SMS.Resources.SAzure.resources
-resgen Sources\Neos.IdentityServer.MultiFactor.SMS.Azure\Resources\SHtml.resx resources\Neos.IdentityServer.MultiFactor.SMS.Resources.SHtml.resources
+resgen Sources\Neos.IdentityServer.MultiFactor.SMS.Azure\Resources\SAzure.resx resources\Neos.IdentityServer.MultiFactor.SMS.Resources.SAzure.en.resources
+resgen Sources\Neos.IdentityServer.MultiFactor.SMS.Azure\Resources\SHtml.resx resources\Neos.IdentityServer.MultiFactor.SMS.Resources.SHtml.en.resources
 
 @PAUSE
